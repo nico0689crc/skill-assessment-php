0fc231a477709d1f47a35e23bb7ce34edc5af152
Nicolas Fernandez <nico.06.89crc@gmail.com>
2024-04-18 13:34:32 +0800

    First commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+echo "Are you sure you want to do this?  Type 'yes' to continue: ";
+echo "\n";
+$handle = fopen ("php://stdin","r");
+$line = fgets($handle);
+if(trim($line) != 'yes'){
+    echo "ABORTING!\n";
+    exit;
+}
+echo "\n";
+echo "Thank you, continuing...\n";
+?>
